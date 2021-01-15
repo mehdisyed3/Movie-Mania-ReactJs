3be2a95c58970aaf78457497afb5d73470ec868c
Syed Mehdi <mehdisyed3@gmail.com>
2021-01-15 14:27:01 -0500

    Header section complete..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import Header from './Header/Header';
 
+
+
+//API Key: 30b033655413f99444343fc0148ecfa4
+  // eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMGIwMzM2NTU0MTNmOTk0NDQzNDNmYzAxNDhlY2ZhNCIsInN1YiI6IjVmN2I4MjE5MDIxY2VlMDAzNTMyMGNiNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZenmcyldCGm-NSZqZbfsxNs5an7XEVcPgVgyDUmo0yY
 function App() {
   return (
     <div className="app">
-      HELLO
+
+      <Header />
     </div>
   );
 }

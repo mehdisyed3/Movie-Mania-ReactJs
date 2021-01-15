f063fe2e9f1292d349efbc2ed4077a965b7bf825
Syed Mehdi <mehdisyed3@gmail.com>
2021-01-15 13:07:16 -0500

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBMZM3AQ_tebpUwWY2G37sNdA4s2tsDkJE",
+  authDomain: "movie-mania-16380.firebaseapp.com",
+  projectId: "movie-mania-16380",
+  storageBucket: "movie-mania-16380.appspot.com",
+  messagingSenderId: "545606334282",
+  appId: "1:545606334282:web:ec3f0595754131862fb3b3"
+};

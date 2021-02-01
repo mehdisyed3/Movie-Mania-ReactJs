5134eb64b972e50da3384cbff57251f8b64e6d57
Syed <mehdisyed3@gmail.com>
2021-02-01 18:08:39 -0500

    Clean commit

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -3,26 +3,23 @@ import VideoCard from '../VideoCard/VideoCard'
 import './Results.css'
 import axios from 'axios'
 import FlipMove from "react-flip-move"
-// import requests from '../requests'
 
 function Results({ selectedOption }) {
   const [movies, setMovies] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
+ useEffect(() => {
+   const fetchData = async () => {
 
-      const req = await axios.get(selectedOption)
+    const req = await axios.get(selectedOption)
 
-      setMovies(req.data.results)
+    setMovies(req.data.results)
 
     }
+   
     return fetchData()
 
 
-
-
   }, [selectedOption])
-
 
   const displayMovies = movies.map(item => <FlipMove><VideoCard key={item.id} movie={item} /></FlipMove> )
 
